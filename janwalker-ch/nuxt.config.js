72f3cd5e4ccf2fdad2721e76a18843fe3c1d0810
Jan <58551692+skyZcoding@users.noreply.github.com>
2022-09-19 13:20:41 +0200

    update config

--- janwalker-ch/nuxt.config.js
+++ janwalker-ch/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'janwalker-ch',
+    title: 'Jan Walker',
     htmlAttrs: {
       lang: 'en'
     },
