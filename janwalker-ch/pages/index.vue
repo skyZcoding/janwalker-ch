7d8141e51964b1ebddb41dc75fd79b7fbec63590
Jan <58551692+skyZcoding@users.noreply.github.com>
2022-09-19 14:48:03 +0200

    fixed setup issues

--- janwalker-ch/pages/index.vue
+++ janwalker-ch/pages/index.vue
@@ -1,6 +1,11 @@
+
 <template>
-  <Tutorial/>
+  <div class="container content-center mx-auto h-screen">
+    <h2 class="text-center font-monos text-xl font-bold">Welcome to the personal website from Jan Walker</h2>
+  </div>
 </template>
+
+
 
 <script lang="ts">
 import Vue from 'vue'
