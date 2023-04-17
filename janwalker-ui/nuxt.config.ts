9c71e4d71f661ac2289a7a36c4d3f9650d6975ab
Jan <58551692+skyZcoding@users.noreply.github.com>
2023-04-17 07:30:25 +0200

    added sitemap module

--- janwalker-ui/nuxt.config.ts
+++ janwalker-ui/nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: true,
-    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/robots'],
-    
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/robots', 'nuxt-simple-sitemap'],
+    runtimeConfig: {
+        public: {
+            siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://janwalker.com',
+        }
+  }
 })
