a98ae4c7bc837c9e59a84fcd8dc214c9fb6a33a4
Jan <58551692+skyZcoding@users.noreply.github.com>
2024-04-29 08:59:41 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/seo']
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/seo'],
+  site: {
+    url: 'https://janwalker.ch',
+    name: 'Jan Walker',
+  },
 })
