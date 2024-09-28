462a1a8cbeeace71e2d2e93c97ce2dea0f38ab43
Jan <58551692+skyZcoding@users.noreply.github.com>
2024-09-28 12:45:12 +0200

    Update nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: true,
-    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/robots', 'nuxt-simple-sitemap', 'nuxt-icon'],
+   ssr: true,
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/robots', 'nuxt-simple-sitemap', 'nuxt-icon', '@nuxtjs/seo'],
     runtimeConfig: {
         public: {
             siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://janwalker.com',
         }
-  }
+  },
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
 })
