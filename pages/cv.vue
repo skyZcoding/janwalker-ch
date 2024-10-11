d3c754a350749b35b8b017409550beb2c94b17e5
Jan <58551692+skyZcoding@users.noreply.github.com>
2024-10-11 12:50:06 +0200

    Add Icons

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import ServerIcon from "#build/components/icon/Backend.vue";
+import BackendIcon from "#build/components/icon/Backend.vue";
+
 const links = [
   {
     title: "Home",
@@ -39,7 +42,7 @@ const links = [
       <div
         class="flex flex-col w-fit justify-center md:pr-8 lg:pr-12 items-center md:items-start"
       >
-        <h1 class="font-bold">
+        <h1>
           Building <span class="text-gradient">Seamless</span> Web Applications
         </h1>
         <p class="text-gray-300 text-base mt-4">
@@ -47,7 +50,7 @@ const links = [
           experience. I love trying out new technologies, mainly in web
           development.
         </p>
-        <Button class="mt-2">Contact Me</Button>
+        <Button class="mt-2">Learn more</Button>
       </div>
       <div
         class="w-auto min-w-96 flex justify-center items-center mt-4 md:mt-0"
@@ -55,7 +58,66 @@ const links = [
         <NuxtImg src="/images/hero.svg" alt="Hero Image" />
       </div>
     </section>
-
+    <section>
+      <h1 class="text-center mb-10">
+        What I <span class="text-gradient">Bring</span> to the Table
+      </h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-2 auto-rows-fr">
+        <Card>
+          <div
+            class="rounded-lg bg-white/10 aspect-square w-fit flex justify-center items-center mb-1"
+          >
+            <IconFrontend class="text-white h-8 w-8 m-2" />
+          </div>
+          <h2 class="mb-2">Frontend Web Development</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+          </p>
+        </Card>
+        <Card>
+          <div
+            class="rounded-lg bg-white/10 aspect-square w-fit flex justify-center items-center mb-1"
+          >
+            <IconBackend class="text-white h-8 w-8 m-2" />
+          </div>
+          <h2 class="mb-2">Backend Development</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+          </p>
+        </Card>
+        <Card>
+          <div
+            class="rounded-lg bg-white/10 aspect-square w-fit flex justify-center items-center mb-1"
+          >
+            <IconDesktop class="text-white h-8 w-8 m-2" />
+          </div>
+          <h2 class="mb-2">Desktop Development</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+          </p>
+        </Card>
+        <Card>
+          <div
+            class="rounded-lg bg-white/10 aspect-square w-fit flex justify-center items-center mb-1"
+          >
+            <IconMobile class="text-white h-8 w-8 m-2" />
+          </div>
+          <h2 class="mb-2">Mobile Device Management</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+          </p>
+        </Card>
+      </div>
+    </section>
+    <section>
+      <h1 class="text-center mb-10">
+        Tech I <span class="text-gradient">Work</span> With
+      </h1>
+    </section>
     <footer class="h-[90rem]"></footer>
   </div>
 </template>
